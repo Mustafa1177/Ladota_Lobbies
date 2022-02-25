131f29edc2f635d883cb9f179e1d8a3cac5e270b
Mustafa1177 <mma15k@hotmail.com>
2022-02-26 00:24:42 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Ladota_Lobbies
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnPSR = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnGames = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnWR = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderWL = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnKDA = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnCreep = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnRank = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelGamesCount = new System.Windows.Forms.Label();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.notifyIconMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStripNotiyIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notificationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStripNotiyIcon.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnNumber,
+            this.columnName,
+            this.columnPSR,
+            this.columnGames,
+            this.columnWR,
+            this.columnHeaderWL,
+            this.columnKDA,
+            this.columnCreep,
+            this.columnRank});
+            this.listView1.ForeColor = System.Drawing.SystemColors.Window;
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(14, 45);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(812, 383);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnNumber
+            // 
+            this.columnNumber.Text = "";
+            this.columnNumber.Width = 43;
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "Name";
+            this.columnName.Width = 156;
+            // 
+            // columnPSR
+            // 
+            this.columnPSR.Text = "PSR";
+            this.columnPSR.Width = 58;
+            // 
+            // columnGames
+            // 
+            this.columnGames.Text = "Games";
+            this.columnGames.Width = 65;
+            // 
+            // columnWR
+            // 
+            this.columnWR.Text = "WR";
+            // 
+            // columnHeaderWL
+            // 
+            this.columnHeaderWL.Text = "W/L";
+            this.columnHeaderWL.Width = 68;
+            // 
+            // columnKDA
+            // 
+            this.columnKDA.Text = "K D A";
+            this.columnKDA.Width = 156;
+            // 
+            // columnCreep
+            // 
+            this.columnCreep.Text = "Creep";
+            this.columnCreep.Width = 124;
+            // 
+            // columnRank
+            // 
+            this.columnRank.Text = "Rank";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DotA lobbies";
+            // 
+            // labelGamesCount
+            // 
+            this.labelGamesCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelGamesCount.AutoSize = true;
+            this.labelGamesCount.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.labelGamesCount.Location = new System.Drawing.Point(14, 439);
+            this.labelGamesCount.Name = "labelGamesCount";
+            this.labelGamesCount.Size = new System.Drawing.Size(132, 16);
+            this.labelGamesCount.TabIndex = 2;
+            this.labelGamesCount.Text = "Retrieving game list...";
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRefresh.AutoSize = true;
+            this.buttonRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRefresh.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.buttonRefresh.Location = new System.Drawing.Point(751, 10);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 28);
+            this.buttonRefresh.TabIndex = 3;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // notifyIconMain
+            // 
+            this.notifyIconMain.ContextMenuStrip = this.contextMenuStripNotiyIcon;
+            this.notifyIconMain.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconMain.Icon")));
+            this.notifyIconMain.Text = "LA-DotA Lobbies";
+            this.notifyIconMain.Visible = true;
+            this.notifyIconMain.Click += new System.EventHandler(this.notifyIcon1_Click);
+            this.notifyIconMain.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStripNotiyIcon
+            // 
+            this.contextMenuStripNotiyIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.notificationToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStripNotiyIcon.Name = "contextMenuStripNotiyIcon";
+            this.contextMenuStripNotiyIcon.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.contextMenuStripNotiyIcon.Size = new System.Drawing.Size(138, 70);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // notificationToolStripMenuItem
+            // 
+            this.notificationToolStripMenuItem.Name = "notificationToolStripMenuItem";
+            this.notificationToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.notificationToolStripMenuItem.Text = "Notification";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(34)))));
+            this.ClientSize = new System.Drawing.Size(839, 474);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.labelGamesCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Window;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "LA-DotA Lobbies";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStripNotiyIcon.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnNumber;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.ColumnHeader columnPSR;
+        private System.Windows.Forms.ColumnHeader columnGames;
+        private System.Windows.Forms.ColumnHeader columnWR;
+        private System.Windows.Forms.ColumnHeader columnHeaderWL;
+        private System.Windows.Forms.ColumnHeader columnKDA;
+        private System.Windows.Forms.ColumnHeader columnCreep;
+        private System.Windows.Forms.ColumnHeader columnRank;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelGamesCount;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.NotifyIcon notifyIconMain;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripNotiyIcon;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem notificationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+//using System.Net.WebSockets;
+
+using Newtonsoft;
+
+namespace Ladota_Lobbies
+{
+    public partial class Form1 : Form
+    {
+        const string LOBBIES_API_URL = "https://eurobattle.net/api/la/live/lobbies";
+        const string STATS_API_URL = "https://eurobattle.net/new/api/la/stats?search={0}";
+        const int QUERY_TIMEOUT = 2000;
+        public class Lobby
+        {
+            public string dotaVariant { get; set; }
+            public string usedSlots { get; set; }
+            public string maxSlots { get; set; }
+            public string gn { get; set; }
+            public List<string> players { get; set; }
+        }
+
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public class Stats
+        {
+            public string name { get; set; }
+            public int rating { get; set; }
+            public int highest_rating { get; set; }
+            public int games { get; set; }
+            public int wins { get; set; }
+            public int loses { get; set; }
+            public int kills { get; set; }
+            public int deaths { get; set; }
+            public int assists { get; set; }
+            public int creepkills { get; set; }
+            public int creepdenies { get; set; }
+            public int neutralkills { get; set; }
+            public int towerkills { get; set; }
+            public int raxkills { get; set; }
+            public int courierkills { get; set; }
+            public string winp { get; set; }
+            public int rank { get; set; }
+        }
+
+
+        List<Lobby> ls = new List<Lobby> { };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayLobbies(ls = GetLobbies());
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason != System.Windows.Forms.CloseReason.WindowsShutDown && e.CloseReason != System.Windows.Forms.CloseReason.ApplicationExitCall)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayLobbies(ls = GetLobbies());
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems.Count > 0)
+                {
+                    var lv = (ListView)sender;
+                    //MessageBox.Show("checking " + lv.SelectedItems[0]);
+                    string playerName = lv.SelectedItems[0].SubItems[1].Text;
+                    var playerStats = GetStats(playerName);
+                    if (playerStats.Count > 0)
+                    {
+                        lv.SelectedItems[0].SubItems[2].Text = playerStats[0].rating.ToString();
+                        lv.SelectedItems[0].SubItems[3].Text = playerStats[0].games.ToString();
+                        lv.SelectedItems[0].SubItems[4].Text = playerStats[0].winp;
+                        lv.SelectedItems[0].SubItems[5].Text = playerStats[0].wins.ToString() + "/" + playerStats[0].loses.ToString();
+                        lv.SelectedItems[0].SubItems[6].Text = string.Format("{0}-{1}-{2}", ((float)playerStats[0].kills / (playerStats[0].wins+playerStats[0].loses)).ToString("0.00"), (playerStats[0].deaths / (playerStats[0].wins + playerStats[0].loses)).ToString("0.00"), (playerStats[0].assists / (playerStats[0].wins + playerStats[0].loses)).ToString("0.00"));
+                        lv.SelectedItems[0].SubItems[7].Text = string.Format("{0}-{1}-{2}", playerStats[0].creepkills.ToString(), playerStats[0].creepdenies.ToString(), playerStats[0].neutralkills.ToString());
+                        lv.SelectedItems[0].SubItems[8].Text = playerStats[0].rank.ToString();
+                        // MessageBox.Show(playerStats[0].name + " " + playerStats[0].rating);
+                    }
+                    else
+                    {
+                        for (int i = 1; i <= 8; i++)
+                        {
+                            lv.SelectedItems[0].SubItems[i].Text = "";
+                        }
+                        MessageBox.Show("Stats unavailable");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message + "   " + ex.StackTrace.ToString());
+            }
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.BringToFront();
+            this.Select();
+
+        }
+
+
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //-------------------------------------------------------------------------------
+        void DisplayLobbies(List<Lobby> lobbies)
+        {
+            listView1.Items.Clear();
+            listView1.Groups.Clear();
+            if (lobbies.Count > 0)
+            {
+                labelGamesCount.Text = String.Format("There are {0} lobbies", lobbies.Count);
+                for (int i = 0; i < lobbies.Count; i++)
+                {
+                    ListViewGroup lvGroup = new ListViewGroup() { Header = String.Format("{0}- {1} [{2}/{3}] {4}", i + 1, lobbies[i].gn, lobbies[i].usedSlots, lobbies[i].maxSlots, lobbies[i].dotaVariant) };
+                    listView1.Groups.Add(lvGroup);
+                    for (int i2 = 0; i2 < lobbies[i].players.Count; i2++)
+                    {
+                        ListViewItem lvItem = new ListViewItem() { Text = (i2 + 1).ToString(), Group = lvGroup };
+                        lvItem.SubItems.Add(new ListViewItem.ListViewSubItem() { Text= lobbies[i].players[i2]});
+                        for(int i3 = 1; i3<= 7; i3++)
+                        {
+                            lvItem.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = "" });
+                        }
+                        listView1.Items.Add(lvItem);
+                    }
+                }
+            }
+            else
+                labelGamesCount.Text = String.Format("There are no open lobbies", lobbies.Count);
+        }
+
+
+
+        //-------------------------------------------------------------------------------
+        public static string GetRemoteContent(string url, int timeout)
+        {
+            string res = "";
+            try
+            {
+                WebRequest request = HttpWebRequest.Create(url);
+                request.Method = "GET";
+                request.Timeout = timeout;
+                WebResponse response = request.GetResponse();
+                System.IO.StreamReader sr = new System.IO.StreamReader(response.GetResponseStream());
+                res = sr.ReadToEnd();
+                sr.Close();
+            }
+            catch (WebException ex) { }
+
+            return res;
+        }
+
+       
+        public static List<Lobby> GetLobbies()
+        {
+            try
+            {
+                var recv = GetRemoteContent(LOBBIES_API_URL, QUERY_TIMEOUT);
+                if (recv == null) recv = "";
+                List<Lobby> result = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Lobby>>(recv);
+                if (result == null) result = new List<Lobby> { };
+                return result;
+            }
+            catch (System.Net.WebException ex)
+            {
+                return new List<Lobby> { };
+            }
+        }
+        public static List<Stats> GetStats(string playerName)
+        {
+            try
+            {
+                var recv = GetRemoteContent(string.Format(STATS_API_URL, playerName), QUERY_TIMEOUT * 2);
+                if (recv == null) recv = "";
+                List<Stats> result = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Stats>>(recv);
+                if (result == null) result = new List<Stats> { };
+                return result;
+            }
+            catch (System.Net.WebException ex)
+            {
+                return new List<Stats> { };
+            }
+        }
+
+
+    }
+}
